4e4dd65802318137689f8c36b922aa112a6ead96
David Thong Nguyen <thnguyen2808@gmail.com>
2018-05-10 08:56:53 +0700

    Linked app to Ionic Pro

--- ionic-app/src/pages/home/home.ts
+++ ionic-app/src/pages/home/home.ts
@@ -1,4 +1,13 @@
-import {Component} from "@angular/core";
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+} from '@ionic-native/google-maps';
+import {Component} from "@angular/core/";
 import {NavController} from "ionic-angular";
 
 @Component({
@@ -6,9 +15,52 @@ import {NavController} from "ionic-angular";
   templateUrl: 'home.html'
 })
 export class HomePage {
+  map: GoogleMap;
 
   constructor(public navCtrl: NavController) {
-
   }
 
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        console.log('Map is ready!');
+
+        // Now you can use all methods safely.
+        this.map.addMarker({
+          title: 'Ionic',
+          icon: 'blue',
+          animation: 'DROP',
+          position: {
+            lat: 43.0741904,
+            lng: -89.3809802
+          }
+        })
+          .then(marker => {
+            marker.on(GoogleMapsEvent.MARKER_CLICK)
+              .subscribe(() => {
+                alert('clicked');
+              });
+          });
+
+      });
+  }
 }
