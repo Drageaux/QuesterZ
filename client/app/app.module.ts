d2e546e2166d5403a1e94e29e17c853d9170ca45
David Thong Nguyen <thnguyen2808@gmail.com>
2018-05-10 10:49:45 +0700

    Trying JS Google Map instead of native SDK

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,40 +1,43 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { HomePage } from '../pages/home/home';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { TabsPage } from '../pages/tabs/tabs';
+import {HomePage} from '../pages/home/home';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Geolocation} from '@ionic-native/geolocation';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    AboutPage,
-    ContactPage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    AboutPage,
-    ContactPage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        AboutPage,
+        ContactPage,
+        TabsPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        AboutPage,
+        ContactPage,
+        TabsPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        Geolocation,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- client/pages/home/home.ts
+++ client/pages/home/home.ts
@@ -1,66 +1,41 @@
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
-  Marker
-} from '@ionic-native/google-maps';
-import {Component} from "@angular/core/";
+import {Component, ElementRef, ViewChild} from "@angular/core";
 import {NavController} from "ionic-angular";
+import {Geolocation} from '@ionic-native/geolocation';
+
+declare var google;
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-  map: GoogleMap;
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
 
-  constructor(public navCtrl: NavController) {
-  }
+    constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+    }
 
-  ionViewDidLoad() {
-    this.loadMap();
-  }
+    ionViewDidLoad() {
+        this.loadMap();
+    }
 
-  loadMap() {
+    loadMap() {
 
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: 43.0741904,
-          lng: -89.3809802
-        },
-        zoom: 18,
-        tilt: 30
-      }
-    };
+        this.geolocation.getCurrentPosition().then((position) => {
 
-    this.map = GoogleMaps.create('map_canvas', mapOptions);
+            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    // Wait the MAP_READY before using any methods.
-    this.map.one(GoogleMapsEvent.MAP_READY)
-      .then(() => {
-        console.log('Map is ready!');
+            let mapOptions = {
+                center: latLng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
 
-        // Now you can use all methods safely.
-        this.map.addMarker({
-          title: 'Ionic',
-          icon: 'blue',
-          animation: 'DROP',
-          position: {
-            lat: 43.0741904,
-            lng: -89.3809802
-          }
-        })
-          .then(marker => {
-            marker.on(GoogleMapsEvent.MARKER_CLICK)
-              .subscribe(() => {
-                alert('clicked');
-              });
-          });
+            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-      });
-  }
+        }, (err) => {
+            console.log(err);
+        });
+
+    }
 }
